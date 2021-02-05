e2b62ae6aaa8cbbfc9d8d3adf9d5e83e56901292
Evelien Snel <evelien.snel@xs4all.nl>
2021-02-05 13:56:50 +0100

    Revered driving direction

--- Robot1.ino
+++ Robot1.ino
@@ -89,18 +89,18 @@ void loop()
 
 void forward()
 {
-  digitalWrite(IN1, HIGH);
-  digitalWrite(IN2, LOW);
-  digitalWrite(IN3, HIGH);
-  digitalWrite(IN4, LOW);
-}
-
-void reverse()
-{
   digitalWrite(IN1, LOW);
   digitalWrite(IN2, HIGH);
   digitalWrite(IN3, LOW);
   digitalWrite(IN4, HIGH);
+}
+
+void reverse()
+{
+  digitalWrite(IN1, HIGH);
+  digitalWrite(IN2, LOW);
+  digitalWrite(IN3, HIGH);
+  digitalWrite(IN4, LOW);
 }
 
 void left()
